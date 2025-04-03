ab8a59c044a1cdb706320910ed6ff44ed77df232
Cristian Falcone <cristianfalcone@hotmail.com>
2025-04-03 11:38:12 -0300

    se agrega controlde errores para boton anterior, falta modularizar y demas aun

--- scripts/index.js
+++ scripts/index.js
@@ -21,9 +21,21 @@ function borrarTarjetas(){
     }
 }
 
+function habilitaDeshabilitaBoton(estado,boton){
+  if (estado==true){
+                boton.disabled=false;
+                   }
+  if (estado==false){
+                boton.disabled=true;
+                    }                 
+
+}
 
 let urlPersonajes="https://rickandmortyapi.com/api/character?page=1";
+
+
 let personajes= await traerPersonajes(urlPersonajes);
+habilitaDeshabilitaBoton(false,anterior);
 console.log(personajes);
 console.log(tarjetas);
 mostrarPersonajes(personajes);
@@ -31,18 +43,30 @@ console.log(personajes.info.next)
 
 siguiente.addEventListener('click', async()=>{
     console.log("ENTRE")
-
     personajes= await traerPersonajes(personajes.info.next);
     console.log(personajes);
     borrarTarjetas();
     mostrarPersonajes(personajes);
+    if (personajes.info.prev!=null){
+                            habilitaDeshabilitaBoton(true,anterior);
+                                   }
 
 })
+
+
 anterior.addEventListener('click', async()=>{
-    personajes= await traerPersonajes(personajes.info.prev);
-    console.log(personajes);
-    borrarTarjetas();
-    mostrarPersonajes(personajes);
+    if (personajes.info.prev!=null){
+      personajes= await traerPersonajes(personajes.info.prev);
+      console.log(personajes.info.prev);
+      borrarTarjetas();
+      mostrarPersonajes(personajes);
+      if (personajes.info.prev==null){
+                          habilitaDeshabilitaBoton(false,anterior);
+                                     }
+                                   } else {
+                                       habilitaDeshabilitaBoton(false,anterior);
+                                          }
+
 
 })
 
