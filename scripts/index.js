5b2566615568d15cd38923cf5e62b7d7032637fe
Cristian Falcone <cristianfalcone@hotmail.com>
2025-04-06 01:15:55 -0300

    se agrega filtrado y filtrado combinado. Falta pulir, optimizar codigo y modularizar

--- scripts/index.js
+++ scripts/index.js
@@ -2,50 +2,39 @@ import { traerPersonajes } from "../API/api.js"
 import { mostrarPersonajes, borrarTarjetas } from "../lib/manejopers.js"
 import {habilitaDeshabilitaBoton} from "../lib/controles.js"
 
-//const contenedor=document.getElementById('CONTENEDOR');
+
 const siguiente=document.getElementById('siguiente');
 const anterior=document.getElementById('anterior');
 
+const filtroGeneral=document.getElementById('filtradoGeneral');
+
+const filtroEstadoVivo=document.getElementById('estadoVivo');
+const filtroEstadoMuerto=document.getElementById('estadoMuerto');
+const filtroEstadoDesconocido=document.getElementById('estadoDesconocido');
+
+const generoFemenino=document.getElementById('generoFemenino');
+const generoMasculino=document.getElementById('generoMasculino');
+const generoSingenero=document.getElementById('generoSingenero');
+const generoDesconocido=document.getElementById('generoDesconocido');
+
+let STATUS="";
+let GENERO="";
+
+filtroEstadoVivo.disabled=true;
+filtroEstadoMuerto.disabled=true;
+filtroEstadoDesconocido.disabled=true;
+
+generoFemenino.disabled=true;
+generoMasculino.disabled=true;
+generoSingenero.disabled=true;
+generoDesconocido.disabled=true;
 
 
-/*function mostrarPersonajes(objeto){
-  let tarjeta=[];
-  for (let i = 0; i < objeto.results.length; i=i+1) {
-       console.log(i);
-       tarjeta[i]=document.createElement('div');
-       tarjeta[i].classList.add('card');
-       tarjeta[i].style.width = '10rem';
-       const cuerpoTarjeta=document.createElement('div');
-       cuerpoTarjeta.classList.add('card-body');
-       const informacionTarjeta=document.createElement('p');
-       informacionTarjeta.classList.add('card-text');
-       const imagen=document.createElement('img');
-       imagen.classList.add('card-img-top');
-       cuerpoTarjeta.appendChild(imagen);
-       cuerpoTarjeta.appendChild(informacionTarjeta);
-       tarjeta[i].appendChild(cuerpoTarjeta);
-       imagen.src=objeto.results[i].image
-       informacionTarjeta.textContent=objeto.results[i].name;
-       contenedor.appendChild(tarjeta[i]);
-  }  
-}*/
 
-/*function borrarTarjetas(){
-          contenedor.innerHTML="";
-}*/
-
-/*function habilitaDeshabilitaBoton(estado,boton){
-  if (estado==true){
-                boton.disabled=false;
-                   }
-  if (estado==false){
-                boton.disabled=true;
-                    }                 
-
-}*/
 
 let urlPersonajes="https://rickandmortyapi.com/api/character?page=1";
 
+//const urlFiltrado="https://rickandmortyapi.com/api/character/?gender=male&status=dead";
 
 let personajes= await traerPersonajes(urlPersonajes);
 habilitaDeshabilitaBoton(false,anterior);
@@ -71,7 +60,6 @@ siguiente.addEventListener('click', async()=>{
                                          }
 })
 
-
 anterior.addEventListener('click', async()=>{
     if (personajes.info.prev!=null){
       personajes= await traerPersonajes(personajes.info.prev);
@@ -88,4 +76,207 @@ anterior.addEventListener('click', async()=>{
                                        habilitaDeshabilitaBoton(false,anterior);
                                           }
 })
+
+filtroEstadoVivo.addEventListener('click',async()=>{
+  if (filtroEstadoVivo.checked){
+             console.log("activo filtrado x Vivos");
+             STATUS="status=alive";
+                               } else {
+                                 console.log("Desactivado filtrado x Vivos");
+                                 STATUS="";
+                                       }
+  borrarTarjetas();
+  habilitaDeshabilitaBoton(false,anterior);
+  habilitaDeshabilitaBoton(true,siguiente);
+  if (GENERO==""){
+    urlPersonajes=`https://rickandmortyapi.com/api/character/?${STATUS}`;
+        } else {
+              urlPersonajes=`https://rickandmortyapi.com/api/character/?${STATUS}&${GENERO}`;
+               }
+  personajes= await traerPersonajes(urlPersonajes);
+  mostrarPersonajes(personajes);
+  console.log(personajes.info.next)                                       
+})
+
+filtroEstadoMuerto.addEventListener('click',async ()=>{
+   if (filtroEstadoMuerto.checked){
+              console.log("activo filtrado x Muertos");
+              STATUS="status=dead";
+                                } else {
+                                  console.log("Desactivado filtrado x Muertos");
+                                  STATUS="";
+                                        }
+  borrarTarjetas();
+  habilitaDeshabilitaBoton(false,anterior);
+  habilitaDeshabilitaBoton(true,siguiente);                                      
+  if (GENERO==""){
+    urlPersonajes=`https://rickandmortyapi.com/api/character/?${STATUS}`;
+        } else {
+              urlPersonajes=`https://rickandmortyapi.com/api/character/?${STATUS}&${GENERO}`;
+               }
+  personajes= await traerPersonajes(urlPersonajes);
+  mostrarPersonajes(personajes);
+  console.log(personajes.info.next)                                       
+})
+
+filtroEstadoDesconocido.addEventListener('click',async ()=>{
+  if (filtroEstadoDesconocido.checked){
+             console.log("activo filtrado x Desconocidos");
+             STATUS="status=unknown";
+                               } else {
+                                 console.log("Desactivado filtrado x Desconocidos");
+                                 STATUS="";
+                                       }
+  borrarTarjetas();
+  habilitaDeshabilitaBoton(false,anterior);
+  habilitaDeshabilitaBoton(true,siguiente);
+  if (GENERO==""){
+              urlPersonajes=`https://rickandmortyapi.com/api/character/?${STATUS}`;
+                  } else {
+                        urlPersonajes=`https://rickandmortyapi.com/api/character/?${STATUS}&${GENERO}`;
+                         }
+  personajes= await traerPersonajes(urlPersonajes);
+  mostrarPersonajes(personajes);
+  console.log(personajes.info.next)                                                      
+})
+
+filtroGeneral.addEventListener('click',async()=>{
+ if (filtroGeneral.checked){
+                filtroEstadoVivo.disabled=false;
+                filtroEstadoMuerto.disabled=false;
+                filtroEstadoDesconocido.disabled=false;
+                generoFemenino.disabled=false;
+                generoMasculino.disabled=false;
+                generoSingenero.disabled=false;
+                generoDesconocido.disabled=false;
+                           } else {
+                            filtroEstadoVivo.checked=false;
+                            filtroEstadoMuerto.checked=false;
+                            filtroEstadoDesconocido.checked=false; 
+                            filtroEstadoVivo.disabled=true;
+                            filtroEstadoMuerto.disabled=true;
+                            filtroEstadoDesconocido.disabled=true;
+
+                            generoFemenino.checked=false;
+                            generoMasculino.checked=false;
+                            generoSingenero.checked=false;
+                            generoDesconocido.checked=false;
+                            generoFemenino.disabled=true;
+                            generoMasculino.disabled=true;
+                            generoSingenero.disabled=true;
+                            generoDesconocido.disabled=true;
+                            
+                            STATUS="";
+                            GENERO="";
+
+                            borrarTarjetas();
+                            habilitaDeshabilitaBoton(false,anterior);
+                            habilitaDeshabilitaBoton(true,siguiente);
+                            urlPersonajes="https://rickandmortyapi.com/api/character?page=1";
+                            personajes= await traerPersonajes(urlPersonajes);
+                            mostrarPersonajes(personajes);
+                            console.log(personajes.info.next)  
+                           }
+
+})
+
+generoFemenino.addEventListener('click',async()=>{
+  if (generoFemenino.checked){
+    console.log("activo filtrado x Femenino");
+    GENERO="gender=female";
+                      } else {
+                        console.log("Desactivado filtrado x Muertos");
+                        GENERO="";
+                              }
+borrarTarjetas();
+habilitaDeshabilitaBoton(false,anterior);
+habilitaDeshabilitaBoton(true,siguiente);  
+console.log(STATUS);
+if (STATUS==""){                                   
+             urlPersonajes=`https://rickandmortyapi.com/api/character/?${GENERO}`;
+              } else {
+                console.log(`entro en FEMENINO + STATUS"  ${STATUS}`);
+                urlPersonajes=`https://rickandmortyapi.com/api/character/?${GENERO}&${STATUS}`;
+                     }
+personajes= await traerPersonajes(urlPersonajes);
+mostrarPersonajes(personajes);
+console.log(personajes.info.next)    
+})
+
+generoMasculino.addEventListener('click',async()=>{
+  if (generoMasculino.checked){
+    console.log("activo filtrado x Femenino");
+    GENERO="gender=male";
+                      } else {
+                        console.log("Desactivado filtrado x Muertos");
+                        GENERO="";
+                              }
+borrarTarjetas();
+habilitaDeshabilitaBoton(false,anterior);
+habilitaDeshabilitaBoton(true,siguiente);  
+console.log(STATUS);
+if (STATUS==""){                                   
+             urlPersonajes=`https://rickandmortyapi.com/api/character/?${GENERO}`;
+              } else {
+                console.log(`entro en FEMENINO + STATUS"  ${STATUS}`);
+                urlPersonajes=`https://rickandmortyapi.com/api/character/?${GENERO}&${STATUS}`;
+                     }
+personajes= await traerPersonajes(urlPersonajes);
+mostrarPersonajes(personajes);
+console.log(personajes.info.next)    
+})
+
+generoSingenero.addEventListener('click',async()=>{
+  if (generoSingenero.checked){
+    console.log("activo filtrado x Femenino");
+    GENERO="gender=genderless";
+                      } else {
+                        console.log("Desactivado filtrado x Muertos");
+                        GENERO="";
+                              }
+borrarTarjetas();
+habilitaDeshabilitaBoton(false,anterior);
+habilitaDeshabilitaBoton(true,siguiente);  
+console.log(STATUS);
+if (STATUS==""){                                   
+             urlPersonajes=`https://rickandmortyapi.com/api/character/?${GENERO}`;
+              } else {
+                console.log(`entro en FEMENINO + STATUS"  ${STATUS}`);
+                urlPersonajes=`https://rickandmortyapi.com/api/character/?${GENERO}&${STATUS}`;
+                     }
+personajes= await traerPersonajes(urlPersonajes);
+mostrarPersonajes(personajes);
+console.log(personajes.info.next)    
+})
+
+generoDesconocido.addEventListener('click',async()=>{
+  if (generoDesconocido.checked){
+    console.log("activo filtrado x Femenino");
+    GENERO="gender=unknown";
+                      } else {
+                        console.log("Desactivado filtrado x Muertos");
+                        GENERO="";
+                              }
+borrarTarjetas();
+habilitaDeshabilitaBoton(false,anterior);
+habilitaDeshabilitaBoton(true,siguiente);  
+console.log(STATUS);
+if (STATUS==""){                                   
+             urlPersonajes=`https://rickandmortyapi.com/api/character/?${GENERO}`;
+              } else {
+                console.log(`entro en FEMENINO + STATUS"  ${STATUS}`);
+                urlPersonajes=`https://rickandmortyapi.com/api/character/?${GENERO}&${STATUS}`;
+                     }
+personajes= await traerPersonajes(urlPersonajes);
+mostrarPersonajes(personajes);
+console.log(personajes.info.next)    
+})
+//evento escucha mouse pasando por las diferentes borrarTarjetas
+/*document.addEventListener('mouseover', ()=>{
+    let dondeEstaAhora=event.target;
+    console.log(dondeEstaAhora);
+    //document.getElementById('elemento-actual').textContent=`El mouse actualmente esta sobre: ${dondeEstaAhora.tagName}`;
+})
+console.log();*/
+
 
